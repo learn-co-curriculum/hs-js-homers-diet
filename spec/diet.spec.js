ab067407e53d86749e33bbe0d50248d30bbb06ba
Ian Candy <iancandysemail@gmail.com>
2015-05-28 10:40:10 -0400

    add spec folder

--- spec/diet.spec.js
+++ spec/diet.spec.js
@@ -1,8 +1,9 @@
 'use strict';
 
-describe('no donuts', function () {
+describe('No Donuts', function () {
 	
 	it('tells Homer good job for not eating donuts', function () {
 		expect(noDonuts(0)).toEqual("Good job, Homer!");
 	})
+	
 })
