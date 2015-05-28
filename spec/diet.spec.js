877416897653034fd06b2eb8331d9b629e8fe1b7
Ian Candy <iancandysemail@gmail.com>
2015-05-28 10:43:23 -0400

    remove solution

--- spec/diet.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('No Donuts', function () {
-	
-	it('tells Homer good job for not eating donuts', function () {
-		expect(noDonuts(0)).toEqual("Good job, Homer!");
-	})
-	
-})
